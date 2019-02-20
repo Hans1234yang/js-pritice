c0500baceaed6c4f7bcc49038da19706264210d0
Hans Yang <545872067@qq.com>
2019-02-20 11:45:58 +0800

    2.19

---
+++ 前端高级/myModule1.js
@@ -0,0 +1,22 @@
+﻿function myModele() {   //34闭包的应用1 
+    //私有数据
+    var msg = 'Hello World';
+
+    function doSomethin() {
+        console.log(msg+'携程旅游');
+    }
+    function doOtherthing() {
+        console.log(msg+'京东购购物');
+    }
+
+
+    //希望在闭包中，不仅仅返回一个函数，而是要返回2个函数
+    //那就返回对象咯
+
+
+    //对外暴露对象(给外部使用)
+    return {
+        something: doSomethin,
+        otherthing: doOtherthing
+    }
+}
---
+++ 前端高级/myModule2js.js
@@ -0,0 +1,19 @@
+﻿//应用了 匿名函数 自调用的原来
+
+(function () {
+    var msg = 'Hello ,';
+
+    function doSomething() {
+        console.log(msg + '  携程旅游');
+    }
+    function doOtherthing() {
+        console.log(msg + '  oppo手机');
+    }
+
+    //直接给window上下文添加属性. 把要添加的方法，添加到这里
+    window.myModule2 = {
+        something: doSomething,
+        Otherthing: doOtherthing
+    };
+
+})();
