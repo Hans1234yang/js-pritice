3b4adb7453e27d98061eb5013794fdd6d8986cb5
Hans Yang <545872067@qq.com>
2019-03-07 21:31:00 +0800

    3月3日收尾

---
+++ 前端高级/worker.js
@@ -0,0 +1,16 @@
+﻿function Feibolaqi(n) {
+    return n <= 2 ? 1 : Feibolaqi(n - 1) + Feibolaqi(n - 2);
+}
+
+//这里的event 是可以接受 主线程向分线程 发送的数据
+var omessage = function (evnet) {
+    var theValue = event.data;
+
+    console.log("分线程从主线程收到的 数据是" + theValue);
+
+    var result = Feibolaqi(theValue);
+
+    postMessage(result);
+    console.log("分线程 往 主线程发送的数据是" + result);
+    
+}
